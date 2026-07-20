8251d83dcf3813a24e669b51257967933ea01379
BikramGhart1 <coludingrain@gmail.com>
2026-07-20 20:24:23 +0545

    fix csrf trusted origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,12 +67,18 @@ MIDDLEWARE = [
     'django_currentuser.middleware.ThreadLocalUserMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "http://localhost:5174",
+#     "http://127.0.0.1:5174",
+# ]
+
+CORS_ALLOWED_ORIGINS = config(
+    'CORS_ALLOWED_ORIGINS',
+    default='http://localhost:5173',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
